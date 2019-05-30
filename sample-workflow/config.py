f2891b322c1ecdb49fdfd0fb6ba7b67e310dec07
Joel Jeyarajah <46009284+joeljeyarajah@users.noreply.github.com>
2019-05-29 22:17:37 -0700

    Add files via upload

---
+++ sample-workflow/config.py
@@ -0,0 +1,13 @@
+# Slack Token
+slackToken = "Bot User Oauth Access token"
+
+# Fully qualified domain name of Portal
+portalFQDN = "Portal URL"
+portalUsername = "Admin Username"
+portalPassword = "Admin Password"
+
+# item ID that portal should look for. 
+#searchItemID = "itemID"
+
+## Required Tags
+standardTags = ['Your', 'Organizations', 'standard', 'tags']
