2a644740d817c28913057fcdd2f54487740f7d58
khibma <khibma@users.noreply.github.com>
2022-02-25 13:03:05 -0500

    developer: azure function

---
+++ Developer/azure/function.python/Hook/hash_check.py
@@ -0,0 +1,31 @@
+import hashlib
+import hmac
+import base64
+
+# Pulling a value from the environment allows you to quickly update the secret key
+# from within Azure, instead of re-deploying the entire function to update the hardcoded value
+#SECRETKEY = os.environ['SECRETKEY'].encode('utf-8')
+SECRETKEY = "MYSECRETKEY"
+
+def crc_response(crc):
+
+    payload = str.encode(crc)
+
+    secret = str.encode(SECRETKEY)
+
+    signature = 'sha256=' + base64.b64encode(hmac.new(secret, payload, 
+                            digestmod=hashlib.sha256).digest()).decode()
+    return signature
+
+
+def verify(payload, xsigHash):
+
+    secret = str.encode(SECRETKEY)
+
+    signature = 'sha256=' + base64.b64encode(hmac.new(secret, payload, 
+                            digestmod=hashlib.sha256).digest()).decode()
+
+    if signature == xsigHash:
+        return True
+    else:
+        return False
